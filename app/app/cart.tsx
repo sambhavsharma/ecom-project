a95d82a0447c22db107c0eac701075527e80c06f
Sambhav Sharma <sambhav.sharma@hotmail.com>
2025-03-24 23:56:47 +0100

    setup expo router

---
+++ app/app/cart.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function CartScreen() {
+    return (
+        <View>
+            <Text>
+                Cart Screen
+            </Text>
+        </View>
+    )
+}
