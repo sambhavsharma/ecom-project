afd3cc1b3874b740fabbb9a8ea2c639717a8d799
Sambhav Sharma <sambhav.sharma@hotmail.com>
2025-03-25 00:21:47 +0100

    first component

---
+++ frontend/app/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, FlatList } from "react-native";
+import products from "../assets/products.json";
+import ProductListItem from "../components/ProductListItem";
+
+
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <FlatList 
+                data={products}
+                renderItem={({item}) => (
+                    <ProductListItem product={item}/>
+                )}
+            />
+        </View>
+    )
+}
---
+++ frontend/components/ProductListItem.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function ProductListItem({product: product}) {
+ 
+    return <Text>{product.name}</Text>
+
+}
