412b16b9f0c0df8825bcfd79c10dd32fca4d3468
Sambhav Sharma <sambhav.sharma@hotmail.com>
2025-03-25 22:00:44 +0100

    product details page

--- frontend/components/ProductListItem.tsx
+++ frontend/components/ProductListItem.tsx
@@ -5,43 +5,32 @@ import { Box } from "./ui/box";
 import { VStack } from "./ui/vstack";
 import { Heading } from "./ui/heading";
 import { Text } from "./ui/text";
+import { Link } from "expo-router";
 
 export default function ProductListItem({product: product}) {
  
     return (
-        <Card className="p-5 rounded-lg max-w-[360px] m-3 flex-1">
-      <Image
-        source={{
-          uri: product.image,
-        }}
-        className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
-        alt="image"
-      />
-      <Text className="text-sm font-normal mb-2 text-typography-700">
-        {product.name}
-      </Text>
-      <VStack className="mb-6">
-        <Heading size="md" className="mb-4">
-          ${product.price}
-        </Heading>
-        {/* <Text size="sm">
-          {product.description}
-        </Text> */}
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Add to cart</ButtonText>
-        </Button>
-        <Button
-          variant="outline"
-          className="px-4 py-2 border-outline-300 sm:flex-1"
-        >
-          <ButtonText size="sm" className="text-typography-600">
-            Wishlist
-          </ButtonText>
-        </Button>
-      </Box>
-    </Card>
-    )
+        <Link href={`product/${product.id}`}>
+            <Card className="p-5 rounded-lg max-w-[360px] m-3 flex-1">
+                <Image
+                    source={{
+                    uri: product.image,
+                    }}
+                    className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+                    alt="image"
+                />
+                <Text className="text-sm font-normal mb-2 text-typography-700">
+                    {product.name}
+                </Text>
+                
+                    <Heading size="md" className="mb-4">
+                    ${product.price}
+                    </Heading>
+                    {/* <Text size="sm">
+                    {product.description}
+                    </Text> */}
 
+            </Card>
+        </Link>
+    )
 }
