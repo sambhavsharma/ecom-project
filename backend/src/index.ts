56f7d8363db5cacaff1e9ffc480aa38075aec194
Sambhav Sharma <sambhav.sharma@hotmail.com>
2025-03-26 23:35:07 +0100

    json parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+import express, {json, urlencoded} from 'express';
 import productRoutes from './routes/products';
+
 const app = express()
 const port = 3000
+app.use(urlencoded({extended: false}));
+app.use(json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.use("/products", productRoutes);
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- backend/src/routes/products/productsController.ts
+++ backend/src/routes/products/productsController.ts
@@ -1,21 +1,22 @@
 import {Request, Response} from "express";
 
 export function listProducts(req: Request, res: Response) {
-    res.send('List of products!')
+    res.send('List of products!');
 }
 
 export function getProduct(req: Request, res: Response) {
-    res.send('Product: ' + req.params.id)
+    res.send('Product: ' + req.params.id);
 }
 
 export function createProduct(req: Request, res: Response) {
-    res.send('Create a product!')
+    console.log(req.body);
+    res.send('Create a product!');
 }
 
 export function updateProduct(req: Request, res: Response) {
-    res.send('Update a product!')
+    res.send('Update a product!');
 }
 
 export function deleteProduct(req: Request, res: Response) {
-    res.send('Delete a product!')
+    res.send('Delete a product!');
 }
